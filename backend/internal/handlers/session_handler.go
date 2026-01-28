e3061448b65d829b9a0da3f0f882bca4b502b28c
Kevin Newbold <kevinwnewbold@gmail.com>
2026-01-28 12:12:03 -0500

    Made dedicated docs_handler.go

--- backend/internal/handlers/session_handler.go
+++ backend/internal/handlers/session_handler.go
@@ -151,34 +151,3 @@ func CreateSessionHTTP(c *fiber.Ctx) error {
 
 	return c.Status(fiber.StatusCreated).JSON(session)
 }
-
-func GetSwaggerSpecification(c *fiber.Ctx) error {
-	html := `
-				<!DOCTYPE html>
-				<html lang="en">
-				<head>
-					<meta charset="UTF-8">
-					<title>Swagger UI</title>
-					<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
-				</head>
-				<body>
-					<div id="swagger-ui"></div>
-					<script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
-					<script>
-						window.onload = function() {
-							SwaggerUIBundle({
-								url: "/api/v1/openapi.yaml",
-								dom_id: '#swagger-ui',
-							})
-						}
-					</script>
-				</body>
-				</html>
-					`
-	c.Set("Content-Type", "text/html")
-	return c.SendString(html)
-}
-
-func GetOpenAPISpec(c *fiber.Ctx) error {
-	return c.SendFile("./docs/openapi.yaml")
-}
